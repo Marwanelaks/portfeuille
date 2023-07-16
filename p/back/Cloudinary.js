0b9d81eb8579bd482ba6f55fde13a632ccb8726a
Marwanelaks <94196561+Marwanelaks@users.noreply.github.com>
2023-07-16 22:27:42 +0200

    Update Cloudinary.js

--- p/back/Cloudinary.js
+++ p/back/Cloudinary.js
@@ -1,9 +1,5 @@
-// const cloudinary = require("cloudinary");
-
 const mailer = require("nodemailer");
-
 require('dotenv').config();
-
 const Sendmail = async(content,nom,email) =>{
    try{
      const transporter = mailer.createTransport({
@@ -25,36 +21,4 @@ const Sendmail = async(content,nom,email) =>{
      console.log(err);
    }
  }
- 
-
 module.exports = {Sendmail}
-
-
-
-
-// cloudinary.config({
-//    cloud_name: process.env.CLOUD_NAME,
-//    api_key: process.env.API_KEY ,
-//    api_secret: process.env.API_SECRET
-// });
-
-// const Uploadimgs = async(imgpath) =>{
-//   try{
-//    const data = await cloudinary.uploader.upload(imgpath,{
-//       resource_type: 'auto',
-//    });
-//    return data;
-//   }catch (err){
-//    return err;
-//   }
-// }
-// const Deleteimgs = async(imgId) =>{
-//    try{
-//     const destroy = await cloudinary.uploader.destroy(imgId);
-//     return destroy;
-//    }catch (err){
-//     return err;
-//    }
-//  }
-
-//  module.exports = {Uploadimgs,Deleteimgs}
