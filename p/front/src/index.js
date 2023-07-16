5f007c3ef53eeb7c2eaabec137f8d7d3ff8c9fb1
Marwanelaks <94196561+Marwanelaks@users.noreply.github.com>
2023-07-16 22:33:05 +0200

    Update index.js

--- p/front/src/index.js
+++ p/front/src/index.js
@@ -2,9 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import NewUser from './OAuth/newUser';
+
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Test from './ChatApp/Test';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -12,8 +12,6 @@ root.render(
     <BrowserRouter>
     <Routes>
       <Route path='/' element={<App/>}/>
-      <Route path='/newUser' element={<NewUser/>} />
-      <Route path='/messages/:username' element={<Test/>} />
     </Routes>
     </BrowserRouter>
   </React.StrictMode>
